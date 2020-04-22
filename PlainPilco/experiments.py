Fixed numerical issues. Run inverted pendulum, mountain and pendulum swing up scenarios succesfully for 10 random seeds

PlainPilco/experiments.py
@@ -4,16 +4,17 @@ import numpy as np
 import gym
 from wrappers import DoublePendWrapper, myPendulum
 
-number_of_random_seeds = 5
+number_of_random_seeds = 10
 
 # Inverted Pendulum
 # env = gym.make('InvertedPendulum-v2')
-# for i in range(5, number_of_random_seeds):
+# for i in range(0, number_of_random_seeds):
 #     name = "inverted_pendulum/"
-#     pilco_run(env, 4, 2, logging=True, eval_runs=10,
-#               restarts=2, eval_max_timesteps=100, seed=i, name=name)
-#
-# # Mountain Car
+#     pilco_run(env, 3, 5, logging=True, eval_runs=10,
+#               eval_max_timesteps=100, seed=i,
+#               fixed_noise=0.001, name=name)
+
+# Mountain Car
 # SUBS=5
 # T = 25
 # for i in range(5, number_of_random_seeds):
@@ -22,7 +23,7 @@ number_of_random_seeds = 5
 #     # Normalise before calling pilco_run
 #     # Initial random rollouts to generate a dataset
 #     X1, Y1, _, _ = rollout(env=env, pilco=None, random=True, timesteps=T, SUBS=SUBS)
-#     for i in range(1,5):
+#     for j in range(1,5):
 #         X1_, Y1_, _, _ = rollout(env=env, pilco=None, random=True,  timesteps=T, SUBS=SUBS)
 #         X1 = np.vstack((X1, X1_))
 #         Y1 = np.vstack((Y1, Y1_))
@@ -32,51 +33,52 @@ number_of_random_seeds = 5
 #     reward = {'type':'exp',
 #               't':np.divide([0.5,0.0] - env.m, env.std),
 #               'W':np.diag([0.5, 0.1])}
-#     pilco_run(env, 5, 5,
+#     pilco_run(env, 4, 5,
 #               SUBS=SUBS,
 #               restarts=3,
-#               maxiter=100,
+#               maxiter=50,
 #               cont=controller,
 #               rew=reward,
 #               sim_timesteps=T,
 #               plan_timesteps=T,
-#               #m_init=np.reshape(np.divide(X1[0,:-1]-env.m, env.std), (1,2)),
-#               #S_init=np.eye(2),
+#               fixed_noise=0.05,
+#               m_init=np.reshape(np.divide(X1[0,:-1]-env.m, env.std), (1,2)),
+#               S_init=0.5 * np.eye(2),
 #               logging=True,
 #               eval_runs=5,
 #               eval_max_timesteps=T,
 #               name=name,
 #               seed=i)
-#
-#
-# Pendulum Swing Up
-env = myPendulum()
-for i in range(5, 10):
-    name = 'pend_swing_up/'
-    controller = {'type':'rbf', 'basis_functions':30, 'max_action':2.0}
-    reward = {'type':'exp', 't':np.array([1.0, 0.0, 0.0]), 'W':np.diag([2.0, 2.0, 0.3])}
-    T = 40
-    name = 'pend_swing_up/' + str(i) + '_'
-    pilco_run(env, 8, 4,
-              SUBS=3,
-              maxiter=50,
-              restarts=2,
-              m_init = np.reshape([-1.0, 0, 0.0], (1,3)),
-              S_init = np.diag([0.01, 0.05, 0.01]),
-              cont=controller,
-              rew=reward,
-              sim_timesteps=T,
-              plan_timesteps=T,
-              logging=True,
-              eval_runs=5,
-              eval_max_timesteps=T,
-              fixed_noise=0.001,
-              name=name,
-              seed=i)
 
-# Inverted Double Pendulum
+
+# Pendulum Swing Up
+# env = myPendulum()
+# for i in range(0, number_of_random_seeds):
+#     name = 'pend_swing_up/'
+#     controller = {'type':'rbf', 'basis_functions':30, 'max_action':2.0}
+#     reward = {'type':'exp', 't':np.array([1.0, 0.0, 0.0]), 'W':np.diag([2.0, 2.0, 0.3])}
+#     T = 40
+#     name = 'pend_swing_up/'
+#     pilco_run(env, 8, 4,
+#               SUBS=3,
+#               maxiter=50,
+#               restarts=2,
+#               m_init = np.reshape([-1.0, 0, 0.0], (1,3)),
+#               S_init = np.diag([0.01, 0.05, 0.01]),
+#               cont=controller,
+#               rew=reward,
+#               sim_timesteps=T,
+#               plan_timesteps=T,
+#               logging=True,
+#               eval_runs=5,
+#               eval_max_timesteps=T,
+#               fixed_noise=0.001,
+#               name=name,
+#               seed=i)
+
+# # Inverted Double Pendulum
 env = DoublePendWrapper()
-for i in range(8, 10):
+for i in range(0, 1):
     controller = {'type':'rbf', 'basis_functions':40, 'max_action':1.0}
     state_dim = 6
     weights = 5.0 * np.eye(state_dim)